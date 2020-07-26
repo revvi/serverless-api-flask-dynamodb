abdbb0468eb14a7bcbf7dc530f679078d53845ee
Revvi Sudirnoputra <revvi.sudirnoputra@gmail.com>
2020-07-26 15:48:14 +0700

    Update code

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 # app.py
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return "Hello and welcome!"
+    return "Hello world!"
+
 
 @app.route("/ping")
 def ping():
-    return "You gave ping! I will give you ping pong!"
+    return { "message": "You gave ping! I will give you ping pong!" }
 
+
+@app.route("/lipsum")
+def go():
+    return render_template("index.html")
